fe7827df07c1c637a97bb200db1ce3d7cb7f3c22
andrew <andrew_zachary@outlook.com>
2021-09-25 00:00:00 +0000

    protect cart against duplication

--- src/store/middleware/TOASTING.js
+++ src/store/middleware/TOASTING.js
@@ -1,14 +1,28 @@
 import {addCartItem} from "../slices/cart";
 import { toastAdded } from "../slices/app";
 
-export default ({ dispatch }) => (next) =>
+export default ({ dispatch, getState }) => (next) =>
     async (action) => {
         if(action.type === addCartItem.type) {
-            dispatch({type: toastAdded.type, payload: {
-                header: "toaster.cart.title",
-                body: "toaster.cart.item_cart_added",
-                time: "item_cart_added.time",
-            }});
+            let itemsInCart = [];
+            getState().cart.items.forEach(item => {
+                if(item.product._id === action.payload.product._id) {
+                    itemsInCart.push(item);
+                }
+            });
+            if(itemsInCart.length > 0) {
+                return dispatch({type: toastAdded.type, payload: {
+                    header: "toaster.cart.title",
+                    body: "toaster.cart.item_cart_exist",
+                    time: "item_cart_added.time",
+                }});
+            } else {
+                dispatch({type: toastAdded.type, payload: {
+                    header: "toaster.cart.title",
+                    body: "toaster.cart.item_cart_added",
+                    time: "item_cart_added.time",
+                }});
+            }
         }
         next(action);
 };
