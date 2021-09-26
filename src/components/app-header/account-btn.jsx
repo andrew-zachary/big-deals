fcb7c0ffed2ea8304b18a2d722c4389e3efc88ad
andrew <andrew_zachary@outlook.com>
2021-09-26 00:00:00 +0000

    account-btn dialog will be closed by clicking any where

--- src/components/app-header/account-btn.jsx
+++ src/components/app-header/account-btn.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from "react-router-dom";
 import {useSelector, useDispatch} from "react-redux";
 import {useTranslation} from 'react-i18next';
@@ -10,6 +10,17 @@ const AccountBtn = () => {
     const dispatch = useDispatch();
     const [toggleDialog, setToggleDialog] = useState(false);
     const currentUserAuth = useSelector((state)=>state.app.currentUserAuth);
+    useEffect(()=>{
+        document.addEventListener('click',(e)=>{
+            if(e.target.classList.contains('btn-toggler-icon') && e.target.classList.contains('fa-times-circle')) {
+                return setToggleDialog(false);
+            }
+            if(e.target.classList.contains('btn-toggler-icon')) {
+                return setToggleDialog(true);
+            }
+            setToggleDialog(false);
+        });
+    }, []);
     return <>
         {
             !currentUserAuth && <li key="account.anony" id="account">
@@ -18,15 +29,15 @@ const AccountBtn = () => {
         }
         {
             currentUserAuth && <li key="account.login" id="account-loggedin">
-                <div id="btn" role="button" onClick={()=>{setToggleDialog(!toggleDialog)}}>
+                <div id="btn-toggler" role="button">
                     <div id="avatar-lg" className="avatar">
-                        {toggleDialog?<i className="fas fa-times-circle"></i>:<i className="fas fa-user-circle"></i>}
+                        {toggleDialog?<i className="fas fa-times-circle btn-toggler-icon"></i>:<i className="fas fa-user-circle btn-toggler-icon"></i>}
                     </div>
                     <div id="avatar-sm" className="avatar">
-                        <i className="fas fa-user-circle"></i>
+                        <i className="fas fa-user-circle btn-toggler-icon"></i>
                     </div>
                     <div id="toggler">
-                        <i className="fas fa-caret-down"></i>
+                        <i className="fas fa-caret-down btn-toggler-icon"></i>
                     </div>
                 </div>
                 <div id="dialog" className={`${toggleDialog?"show ":"hide "} text-center`}>
