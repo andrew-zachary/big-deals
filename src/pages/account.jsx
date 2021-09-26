68f4c22fedb6846de6084f38ae4ae6f4a34cc1cb
andrew <andrew_zachary@outlook.com>
2021-09-26 00:00:00 +0000

    loggedin will redirect to orders

--- src/pages/account.jsx
+++ src/pages/account.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import {Route, Redirect} from 'react-router-dom';
 import Login from "../components/account/login.jsx";
 import Register from "../components/account/register.jsx";
 
 const Account = () => {
+    const currentUserAuth = useSelector(state=>state.app.currentUserAuth);
     return (
-        <section id="account-page">
-            <div id="account-page-container" className="d-flex justify-content-center align-items-center">
-                <Route exact path="/account" component={Login} />
-                <Route path="/account/register" component={Register} />
-            </div>
-        </section>
+        <>
+            {
+                !currentUserAuth && <section id="account-page">
+                    <div id="account-page-container" className="d-flex justify-content-center align-items-center">
+                        <Route exact path="/account" component={Login} />
+                        <Route path="/account/register" component={Register} />
+                    </div>
+                </section>
+            }
+            {
+                currentUserAuth && <Redirect to="/orders" />
+            }
+        </>
     );
 };
 
