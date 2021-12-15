6005a1b9dd8f1c473ad6425a271aedf791f24591
andrew <andrew_zachary@outlook.com>
2021-12-15 00:00:00 +0000

    burger-btn w-12 h-1

--- src/layout/burger-btn.jsx
+++ src/layout/burger-btn.jsx
@@ -3,9 +3,9 @@ import { layoutContext, layoutActions } from './context.js';
 
 const BurgerBtn = () => {
     const {state, dispatch} = useContext(layoutContext);
-    const hamburgerLine = `h-2 w-14 my-1.5 rounded-full bg-white`;
+    const hamburgerLine = `h-1 w-12 my-1.5 rounded-full bg-white`;
     return <button
-        className="p-8 rounded-full bg-primary shadow-secondary inline-block"
+        className="p-6 rounded-full bg-primary shadow-secondary inline-block"
         onClick={()=>dispatch({type: layoutActions.TOGGLE_MENU, payload: {toggle: !state.menu.toggle}})}
     >
         <div id="button-inner" className='flex flex-col justify-center items-center'>
