917fcb5bd9703de700cf584e6853095bcbd34760
andrew <andrew_zachary@outlook.com>
2021-12-17 00:00:00 +0000

    w#form-is-not-connected moved btn(type=btn) outside form

--- src/pages/login-form.jsx
+++ src/pages/login-form.jsx
@@ -32,9 +32,9 @@ const LoginForm = ({setCurrentForm}) => {
             <BDFormInput id="email" name="email" type="text" label="email" value={loginForm.values.email} onChange={loginForm.handleChange} onBlur={loginForm.handleBlur} errors={loginForm.errors} touched={loginForm.touched} />
             <BDFormInput id="password" name="password" type="password" label="password" value={loginForm.values.password} onChange={loginForm.handleChange} onBlur={loginForm.handleBlur} errors={loginForm.errors} touched={loginForm.touched} />
             <button type='submit' className='font-ssp font-regular text-4xl bg-primary shadow-secondary text-white px-8 py-4 rounded-[0.4rem] uppercase w-full' disabled={!loginForm.isValid}>sign in</button>
-            <button className='font-ssp font-regular text-4xl capitalize mb-12 mt-14' onClick={()=>setCurrentForm('register')}>sign up&nbsp;</button>
-            <button className='font-ssp font-regular text-4xl capitalize mb-12 mt-14' onClick={()=>setCurrentForm('forgot-password')}>/&nbsp;forgot password</button>
         </form>
+        <button className='font-ssp font-regular text-4xl capitalize mb-12 mt-14' onClick={()=>setCurrentForm('register')}>sign up&nbsp;</button>
+        <button className='font-ssp font-regular text-4xl capitalize mb-12 mt-14' onClick={()=>setCurrentForm('forgot-password')}>/&nbsp;forgot password</button>
     </>
 };
 
--- src/pages/register-form.jsx
+++ src/pages/register-form.jsx
@@ -16,7 +16,6 @@ const RegisterForm = ({setCurrentForm}) => {
         validate:(values) => {
             let errors = {};
             const result = RegisterSchema.validate(values,{abortEarly: false});
-            console.log(result);
             if(result.error) {
                 result.error.details.forEach(errorItem => {
                     errors[errorItem.context.label] = errorItem.message;
@@ -36,8 +35,8 @@ const RegisterForm = ({setCurrentForm}) => {
             <BDFormInput id="password" name="password" type="password" label="password" value={registerForm.values.password} onChange={registerForm.handleChange} onBlur={registerForm.handleBlur} errors={registerForm.errors} touched={registerForm.touched} />
             <BDFormInput id="password_confirm" name="passwordConfirm" type="password" label="confirm password" value={registerForm.values.passwordConfirm} onChange={registerForm.handleChange} onBlur={registerForm.handleBlur} errors={registerForm.errors} touched={registerForm.touched} />
             <button type='submit' className='font-ssp font-regular text-4xl bg-primary shadow-secondary text-white px-8 py-4 rounded-[0.4rem] uppercase w-full' disabled={!registerForm.isValid}>sign up</button>
-            <button className='font-ssp font-regular text-4xl mb-12 mt-14' onClick={()=>setCurrentForm('login')}>have account - sign in</button>
         </form>
+        <button className='font-ssp font-regular text-4xl mb-12 mt-14' onClick={()=>setCurrentForm('login')}>have account - sign in</button>
     </>
 };
 
