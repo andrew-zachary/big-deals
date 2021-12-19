f52f7ff26e0485a5f40641c54571bdce5d684bc4
andrew <andrew_zachary@outlook.com>
2021-12-19 00:00:00 +0000

    test herokuapp

--- src/store/middlewares/api.js
+++ src/store/middlewares/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { apiStartCall } from "../actions.js";
 
-const axiosClient = axios.create({baseURL: 'http://localhost:3000/api'});
+const axiosClient = axios.create({baseURL: 'https://andrew-zachary-dev.herokuapp.com/api'});
 
 export default ({dispatch}) => (next) => async (action) => {
     if(action.type !== apiStartCall.type) return next(action);
