e23b456ece9edc3e05aac4e04e8c62442f191123
andrew <andrew_zachary@outlook.com>
2021-12-22 00:00:00 +0000

    fix#1 end-points deal.js, product.js

--- src/store/end-points/deal.js
+++ src/store/end-points/deal.js
@@ -2,7 +2,7 @@ import { itemsReceived } from "../slices/deal.js";
 
 const baseDealUrl = '/bd/deals';
 
-const getAllDeals = (lastPage=1, limit=5) => {
+const getAllDeals = (params={}, lastPage=1, limit=5) => {
     return {
         method: 'GET',
         url: `${baseDealUrl}/?page=${lastPage}&limit=${limit}`,
--- src/store/end-points/product.js
+++ src/store/end-points/product.js
@@ -2,7 +2,7 @@ import { itemsReceived } from "../slices/product.js";
 
 const baseDealUrl = '/bd/products';
 
-const getAllProducts = (lastPage=1, limit=5) => {
+const getAllProducts = (params={}, lastPage=1, limit=5) => {
     return {
         method: 'GET',
         url: `${baseDealUrl}/?page=${lastPage}&limit=${limit}`,
