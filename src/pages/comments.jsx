00f39151fde453200f247b6091015829658ab8eb
andrew <andrew_zachary@outlook.com>
2021-12-22 00:00:00 +0000

    extract product id with useParams

--- src/pages/comments.jsx
+++ src/pages/comments.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import {getProductComments} from '../store/end-points/comment.js'
 import InfiniteScrollList from '../components/app/infinite-scroll-list.jsx';
 import CommentListItem from '../components/comments-page/comment-list-item.jsx';
 
 const CommentsPage = () => {
+    const params = useParams();
     const {commentsItems, commentsLastPage, commentsHasMore} = useSelector(state=>state.comment);
-    const test = {
-        "avgRating": {
-            "amount": 1.875,
-            "numOfRating": 3
-        },
-        "_id": "61ab7506478f296a8ad4735b",
-        "seller": "61ab73e0478f296a8ad47343",
-        "name": "name7",
-        "features": [
-            "feature1",
-            "feature2",
-            "feature3"
-        ],
-        "desc": "desc7",
-        "price": 17.05,
-        "instock": true,
-        "freeShipping": true,
-        "__v": 0
-    };
     return <div id="comments-page" className="flex flex-col px-4 max-w-screen-sm mx-auto h-full">
         <InfiniteScrollList
             endPointOptions={getProductComments}
@@ -33,7 +16,7 @@ const CommentsPage = () => {
             lastPage={commentsLastPage}
             ItemComponent={CommentListItem} 
             pickedMode="comments" 
-            params={{productId: test._id}}/>
+            params={{productId: params.id}}/>
     </div>
 };
 
