8ba8c1caf839b4780d621dde8b03a65ba18f149e
andrew <andrew_zachary@outlook.com>
2021-12-23 00:00:00 +0000

    extract circle-rating component, style comments list

---
+++ src/components/comments-page/circle-rating.jsx
@@ -0,0 +1,16 @@
+import React, {useEffect, useRef} from 'react';
+
+const CircleRating = ({amount}) => {
+    const ratingCircle = useRef();
+    useEffect(()=>{
+        ratingCircle.current.setAttribute('stroke-dasharray', `${100*(amount/5)} 100`);
+    }, []);
+    return <svg className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] h-full w-full" viewBox="0 0 36 36">
+        <path d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" strokeLinecap="round" stroke="#fcd6b0" strokeWidth="1" fill="none" strokeDasharray="100, 100" />
+        <path d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" ref={ratingCircle} id="rating-circle"  strokeLinecap="round" stroke="#f68b1e" strokeWidth="1" fill="none" strokeDasharray="0, 100" />
+    </svg>
+};
+
+CircleRating.displayName = "circle rating";
+
+export default CircleRating;
--- src/components/comments-page/comments-header.jsx
+++ src/components/comments-page/comments-header.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactStars from 'react-rating-stars-component';
 import {FaChevronDown} from 'react-icons/fa';
+import CircleRating from './circle-rating.jsx';
 
 const CommentsHeader = ({openCommentsList}) => {
     const {state:{product}} = useLocation();
-    console.log(product);
+
     return <section>
         <div id='rate' className='text-center'>
-            <h1 className='text-8xl font-mon font-medium p-[6rem] rounded-full border-4 border-primary inline-block relative'>
+            <h1 className='text-8xl font-mon font-medium p-[8rem] inline-block relative'>
+                <CircleRating amount={product.avgRating.amount} />
                 <span className='absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]'>{parseFloat(product.avgRating.amount).toFixed(1)}</span>
             </h1>
             <h2 className='text-5xl font-mon font-medium mt-6 text-primary'>{product.avgRating.numOfRating} reviews</h2>
@@ -22,7 +24,7 @@ const CommentsHeader = ({openCommentsList}) => {
                 color="#f9ae6280"
                 classNames=""
             />
-            <button className='text-6xl text-primary px-4 py-4 mt-8' onClick={()=>openCommentsList(true)}><FaChevronDown /></button>
+            <button className='text-6xl text-primary mt-8' onClick={()=>openCommentsList(true)}><FaChevronDown /></button>
         </div>
     </section>
 };
