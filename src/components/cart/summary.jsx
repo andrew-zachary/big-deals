3c2d4cef55bfa4958478749883ad6873d6273bc0
andrew <andrew_zachary@outlook.com>
2021-12-24 00:00:00 +0000

    Cart Summary, items number, total cost

---
+++ src/components/cart/summary.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {FaShoppingCart} from 'react-icons/fa'
+
+const CartSummary = () => {
+    return <section id='cart-summary' className='bd-unit'>
+        <div className='flex flex-col items-center w-full'>
+            <div className='flex items-center'>
+                <FaShoppingCart className='text-6xl text-primary' />
+                <div className='relative p-6 bg-primary rounded-full'>
+                    <span className='text-3xl text-white absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]'>0</span>
+                </div>
+            </div>
+            <div className='text-4xl flex items-center font-mont font-medium'>
+                <span>0</span>
+                <span className='bd-unit'>$</span>
+            </div>
+        </div>
+    </section>
+};
+
+CartSummary.displayName = "cart summary";
+
+export default CartSummary;
--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import BurgerBtn from './burger-btn.jsx';
+import CartSummary from '../components/cart/summary.jsx';
 
 const LayoutHeader = () => {
     return <header>
         <div className='max-w-app-body mx-auto flex items-center h-full'>
             <BurgerBtn />
+            <CartSummary />
         </div>
     </header>
 }
