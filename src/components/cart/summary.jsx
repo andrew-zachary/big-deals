dbb83c421aa4ac0bdc25da1cec0b9974de4b16cd
andrew <andrew_zachary@outlook.com>
2021-12-25 00:00:00 +0000

    rename burger-btn to menu-btn, restyle cart summary

--- src/components/cart/summary.jsx
+++ src/components/cart/summary.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { FaShoppingCart } from 'react-icons/fa';
 
 const CartSummary = () => {
     const {totalItemsNumber, totalItemsCost} = useSelector(state=>state.cart);
@@ -10,10 +9,9 @@ const CartSummary = () => {
                 <div className='relative p-6 bg-primary rounded-full'>
                     <span className='text-3xl text-white absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]'>{totalItemsNumber}</span>
                 </div>
-                <FaShoppingCart className='text-6xl text-primary' />
             </div>
             <div className='bd-unit'>
-                <div className='text-primary text-5xl flex items-center font-mont font-medium'>
+                <div className='bg-primary text-white text-4xl px-8 py-4 flex items-center font-mont font-medium rounded-full'>
                     <span>$</span>
                     <span className='bd-unit'>{totalItemsCost.toFixed(2)}</span>
                 </div>
