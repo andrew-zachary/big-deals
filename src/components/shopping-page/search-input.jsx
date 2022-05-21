791878293612dc912ccab6f6fe135c76620fa4df
andrew <andrew_zachary@outlook.com>
2022-05-21 00:00:00 +0000

    auto-complete-list ui setup

--- src/components/shopping-page/search-input.jsx
+++ src/components/shopping-page/search-input.jsx
@@ -1,13 +1,23 @@
 import React, {useRef} from 'react';
+import SimpleBar from 'simplebar-react';
 
 import { FaSearch, FaSortDown } from 'react-icons/fa';
 
-const SearchInput = ({searchHandler, showModeList, pickedMode, setPickedMode}) => {
+const SearchInput = ({resetSearchHandler, autoComplete, autoCompleteHandler, searchHandler, showModeList, pickedMode, setPickedMode}) => {
     const searchTxt = useRef('');
     const pickingModes = [
         {value: 'deals', header: 'search deals', desc: 'every day new deal is out ... '},
         {value: 'products', header: 'search products', desc: 'browse all products'}
     ];
+    const pickAutoComplete = (e) => {
+        const searchValue = e.target.innerHTML;
+        searchTxt.current.value = searchValue;
+        searchHandler(searchValue);
+    }
+    const modePicked = (mode) => {
+        resetSearchHandler();
+        setPickedMode(mode);
+    }
 
     return <div id="search-input" className='w-full flex flex-col'>
         <div id="search-input-ctrls" className='w-full flex'>
@@ -21,7 +31,8 @@ const SearchInput = ({searchHandler, showModeList, pickedMode, setPickedMode}) =
                 type="text" 
                 placeholder={`${showModeList === 'show'?'...':'search'+' '+pickedMode}`} 
                 className='focus:outline-none text-4xl w-4/5 capitalize border-y-2 border-primary dark:border-primary-dark dark:bg-[#1e293b]'
-                onKeyDown={(e)=> e.key === 'Enter'?searchHandler(e.target.value):null} 
+                onKeyDown={(e)=> e.key === 'Enter'?searchHandler(e.target.value):null}
+                onInput={(e)=> autoCompleteHandler(e.target.value)}
             />
             <button
                 id="search-btn"
@@ -31,10 +42,22 @@ const SearchInput = ({searchHandler, showModeList, pickedMode, setPickedMode}) =
             </button>
         </div>
         {
-            showModeList && <ul id="mode-pickup-list" className="w-full block border-primary dark:border-primary-dark border-x border-b-4">
+            autoComplete && <SimpleBar id='auto-complete-list' className='p-5 border-primary dark:border-primary-dark border-x border-b-4'>
+                <ul className='w-full block'>
+                    <li onClick={(e)=> pickAutoComplete(e)} className='p-4 text-black dark:text-white text-4xl hover:text-white hover:bg-primary hover:dark:bg-primary-dark cursor-pointer'>20</li>
+                    <li onClick={(e)=> pickAutoComplete(e)} className='p-4 text-black dark:text-white text-4xl hover:text-white hover:bg-primary hover:dark:bg-primary-dark cursor-pointer'>1</li>
+                    <li onClick={(e)=> pickAutoComplete(e)} className='p-4 text-black dark:text-white text-4xl hover:text-white hover:bg-primary hover:dark:bg-primary-dark cursor-pointer'>3</li>
+                    <li onClick={(e)=> pickAutoComplete(e)} className='p-4 text-black dark:text-white text-4xl hover:text-white hover:bg-primary hover:dark:bg-primary-dark cursor-pointer'>0</li>
+                    <li onClick={(e)=> pickAutoComplete(e)} className='p-4 text-black dark:text-white text-4xl hover:text-white hover:bg-primary hover:dark:bg-primary-dark cursor-pointer'>11</li>
+                    <li onClick={(e)=> pickAutoComplete(e)} className='p-4 text-black dark:text-white text-4xl hover:text-white hover:bg-primary hover:dark:bg-primary-dark cursor-pointer'>pro</li>
+                </ul>
+            </SimpleBar>
+        }
+        {
+            showModeList && <ul id='mode-pickup-list' className='w-full block border-primary dark:border-primary-dark border-x border-b-4'>
                 {
                     pickingModes.map(mode => {
-                        return <li key={mode.value} className="cursor-pointer mt-4 capitalize hover:bg-primary dark:hover:bg-primary-dark hover:text-white" onClick={()=>setPickedMode(mode.value)}>
+                        return <li key={mode.value} className="cursor-pointer mt-4 capitalize hover:bg-primary dark:hover:bg-primary-dark hover:text-white" onClick={()=>modePicked(mode.value)}>
                             <h1 className="text-3xl">{mode.header}</h1>
                             <h2 className="text-2xl">{mode.desc}</h2>
                         </li>
--- src/pages/shopping.jsx
+++ src/pages/shopping.jsx
@@ -14,26 +14,59 @@ const ShoppingPage = () => {
     const dispatch = useDispatch();
     const [triggerClicked] = useClickInOut('pick-mode-trigger', document);
     const [pickedMode, setPickedMode] = useState('products');
+    const [autoComplete, startAutoComplete] = useState(false);
     const [startSearch, setStartSearch] = useState({
         show: false,
         value: '',
         mode: ''
     });
+    const autoCompleteHandler = (searchTxt) => {
+        if(searchTxt.length >= 3) {
+            startAutoComplete(true);
+        } else {
+            startAutoComplete(false);
+        }
+    }
     const searchHandler = (searchTxt) => {
+        //close auto-complete-panel
+        startAutoComplete(false);
+        //if no text return
+        if(searchTxt === '') return;
+        //if searchTxt changed reset search and start new searching
         if(startSearch.value !== searchTxt) {
             dispatch({type: resetProductsSearch.type});
             dispatch({type: resetDealsSearch.type});
+            setStartSearch({
+                show: true, 
+                value: searchTxt, 
+                mode: pickedMode
+            });
+        //else show last result
+        } else {
+            setStartSearch((value)=>({...value, show:true}));
         }
-        setStartSearch({
-            show: true, 
-            value: searchTxt, 
-            mode: pickedMode
-        });
     };
+    const resetSearchHandler = () => {
+        dispatch({type: resetProductsSearch.type});
+        dispatch({type: resetDealsSearch.type});
+        setStartSearch({
+            show: false,
+            value: '',
+            mode: ''
+        });
+    }
 
     return <div id="shopping-page" className='relative flex flex-col px-4 max-w-screen-sm mx-auto h-full'>
         { startSearch.show && <SearchingPanel pickedMode={pickedMode} startSearch={startSearch} setStartSearch={setStartSearch} /> }
-        <SearchInput searchHandler={searchHandler} showModeList={triggerClicked} pickedMode={pickedMode} setPickedMode={setPickedMode} />
+        <SearchInput 
+            searchHandler={searchHandler} 
+            showModeList={triggerClicked} 
+            pickedMode={pickedMode} 
+            setPickedMode={setPickedMode}
+            autoComplete={autoComplete}
+            autoCompleteHandler={autoCompleteHandler}
+            resetSearchHandler={resetSearchHandler}
+        />
         <ListSwitch pickedMode={pickedMode} />
     </div>
 };
