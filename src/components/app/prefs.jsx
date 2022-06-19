0f3f96ead0a3d8523de2cb01068d192b107dc0f8
andrew <andrew_zachary@outlook.com>
2022-06-19 00:00:00 +0000

    AppPrefs will trigger resetDealsSearch/resetProductsSearch when lang change

--- src/components/app/prefs.jsx
+++ src/components/app/prefs.jsx
@@ -1,14 +1,21 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+
+import { resetDealsSearch } from '../../store/slices/deal.js';
+import { resetProductsSearch } from '../../store/slices/product.js';
 
 const AppPrefs = () => {
+    const dispatch = useDispatch();
     const { i18n } = useTranslation();
     const { mode, lang } = useSelector(state=>state.user.userInfo.appPreferences);
     const htmlEle = document.querySelector('html');
     useEffect(()=> {
         i18n.changeLanguage(lang);
         htmlEle.setAttribute('dir', lang === 'en'?'ltr':'rtl');
+        dispatch({type: resetDealsSearch.type});
+        dispatch({type: resetProductsSearch.type});
     }, [lang]);
     useEffect(()=> {
         if(mode === 'dark') {
