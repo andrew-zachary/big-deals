faf7ac6dc6b0cd3cdf15f17a6c065b7ce65f8caa
andrew <andrew_zachary@outlook.com>
2022-09-22 00:00:00 +0000

    update api-url to onrender.com

--- src/utilities/axiosClient.js
+++ src/utilities/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = window.location.host === 'localhost:8000'?'http://localhost:3000/api':'https://andrew-zachary-dev.herokuapp.com/api';
+const baseURL = window.location.host === 'localhost:8000'?'http://localhost:3000/api':'https://andrew-zachary-dev.onrender.com/api';
 const axiosClient = axios.create({
     baseURL,
     withCredentials: true
