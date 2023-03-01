b66fd6ab8a3ad7be470cfa1ff7df5e23c342b02a
andrew-zachary <andrew_zachary@outlook.com>
2023-03-01 00:00:00 +0000

    fix circle rating animation using setTimeout

--- src/components/comments-page/circle-rating.jsx
+++ src/components/comments-page/circle-rating.jsx
@@ -1,9 +1,11 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useLayoutEffect, useRef} from 'react';
 
 const CircleRating = ({amount}) => {
-    const ratingCircle = useRef();
-    useEffect(()=>{
-        ratingCircle.current.setAttribute('stroke-dasharray', `${100*(amount/5)} 100`);
+    const ratingCircle = useRef(null);
+    useLayoutEffect(()=>{
+        setTimeout(() => {
+            ratingCircle.current.setAttribute('stroke-dasharray', `${100*(amount/5)} 100`);
+        }, 0);
     }, []);
     return <svg className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] h-full w-full" viewBox="0 0 36 36">
         <path d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" strokeLinecap="round" stroke="#fcd6b0" strokeWidth="1" fill="none" strokeDasharray="100, 100" />
