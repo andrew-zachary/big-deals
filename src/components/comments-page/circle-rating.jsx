ee11be96cd5f40f8f99ce02a96ade2c3fbf58d4d
andrew-zachary <andrew_zachary@outlook.com>
2023-03-02 00:00:00 +0000

    remove rating-circle/animation-fix by init strokeDashoffset=100 and animate stroke-dashoffset=totalLength - (100*(amount/5))

--- src/components/comments-page/circle-rating.jsx
+++ src/components/comments-page/circle-rating.jsx
@@ -3,13 +3,22 @@ import React, {useLayoutEffect, useRef} from 'react';
 const CircleRating = ({amount}) => {
     const ratingCircle = useRef(null);
     useLayoutEffect(()=>{
-        setTimeout(() => {
-            ratingCircle.current.setAttribute('stroke-dasharray', `${100*(amount/5)} 100`);
-        }, 0);
+        const totalLength = ratingCircle.current.getTotalLength();
+        const lengthToAnimate = totalLength - (100*(amount/5));
+        ratingCircle.current.setAttribute('stroke-dasharray', totalLength);
+        ratingCircle.current.setAttribute('stroke-dashoffset', lengthToAnimate);
     }, []);
     return <svg className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] h-full w-full" viewBox="0 0 36 36">
         <path d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" strokeLinecap="round" stroke="#fcd6b0" strokeWidth="1" fill="none" strokeDasharray="100, 100" />
-        <path d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" ref={ratingCircle} id="rating-circle"  strokeLinecap="round" stroke="var(--primary)" strokeWidth="1" fill="none" strokeDasharray="0, 100" />
+        <path d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" 
+            id="rating-circle"
+            ref={ratingCircle} 
+            strokeLinecap="round" 
+            stroke="var(--primary)" 
+            strokeWidth="1" 
+            fill="none"
+            strokeDashoffset="100"
+        />
     </svg>
 };
 
