7209fe5495e45446f9878ee4130ddd8b4afdfe41
andrew-zachary <andrew_zachary@outlook.com>
2023-04-10 00:00:00 +0000

    animate__heartBeat for cart

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { ref } from "vue";
+    import { ref, watch } from "vue";
 
     import useCartStore from "../stores/cart";
     import { useTranslate } from "../composables/useTranslate";
@@ -10,6 +10,16 @@
     const cartStore = useCartStore();
     const { doTranslate } = useTranslate();
     const openCart = ref(false);
+    const cartRef = ref(null);
+
+    watch(() => cartStore.totalItems, () => {
+        cartRef.value.classList.add('animate__heartBeat');
+    });
+
+    const removeTadaClass = () => {
+        cartRef.value.classList.remove('animate__heartBeat');
+    };
+
 </script>
 
 <template>
@@ -19,7 +29,9 @@
         cursor-pointer"
         @click="openCart = true"
     >
-        <Badge :value="cartStore.totalItems"/>
+        <div id="animation-box" class="inline-block animate__animated" @animationend="removeTadaClass" ref="cartRef">
+            <Badge :value="cartStore.totalItems" />
+        </div>
     </div>
     <Dialog v-model:visible="openCart" modal header="Header" :closable="false">
         <template #header>
@@ -72,6 +84,43 @@
     border-radius: 0!important;
     @apply bg-primary-bg dark:bg-primary-bg-dark #{!important};
   }
+}
+
+.animate__animated {
+  animation-duration: 0.8s;
+  animation-fill-mode: both;
+}
+
+.animate__heartBeat {
+  animation-name: heartBeat;
+  animation-timing-function: ease-in-out;
+}
+
+@keyframes heartBeat {
+    0% {
+        -webkit-transform: scale(1);
+        transform: scale(1);
+    }
+
+    14% {
+        -webkit-transform: scale(1.3);
+        transform: scale(1.3);
+    }
+
+    28% {
+        -webkit-transform: scale(1);
+        transform: scale(1);
+    }
+
+    42% {
+        -webkit-transform: scale(1.3);
+        transform: scale(1.3);
+    }
+
+    70% {
+        -webkit-transform: scale(1);
+        transform: scale(1);
+    }
 }
     
 </style>
